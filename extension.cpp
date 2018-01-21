fff24cce7428e80ec61dc10711b5de5b6c2a34b5
Fábián Tamás László <giganetom@gmail.com>
2018-01-21 15:10:47 +0100

    Include C++ build chain

---
+++ extension.cpp
@@ -0,0 +1,8 @@
+#include "pxt.h"
+using namespace pxt;
+namespace sbrick {
+    //%
+    int extfun(int x, int y) {
+        return x + y;
+    }
+}
---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace sbrick {
+    /**
+     * Help goes here.
+     */
+    //% shim=sbrick::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
